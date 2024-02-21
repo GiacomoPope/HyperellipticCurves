4502c8237df2a94ee31c392c43416f2a4004523a
Giacomo Pope <giacomopope@gmail.com>
2024-02-21 17:51:17 +0000

    first commit

---
+++ divisor_split.py
@@ -0,0 +1,126 @@
+from jacobian_split import JacobianSplit
+
+class MumfordDivisorSplit():
+    def __init__(self, parent, u, v):
+
+        if not isinstance(parent, JacobianSplit):
+            raise TypeError(f"parent must be of type ")
+        if not isinstance(u, Polynomial) or not isinstance(v, Polynomial):
+            raise TypeError(f"arguments {u = } and {v = } must be polynomials")
+
+        self._parent = parent
+        self._u = u
+        self._v = v
+
+    def parent(self):
+        return self._parent
+
+    def __repr__(self) -> str:
+        return "({self._u}, {self._v})"
+
+    def uv(self):
+        return (self._u, self._v)
+
+
+    def cantor_composition(self, other):
+        """
+        Follows algorithm 3.4 of
+
+        Efficient Arithmetic on Hyperelliptic Curves With Real Representation
+        David J. Mireles Morales (2008)
+        https://www.math.auckland.ac.nz/~sgal018/Dave-Mireles-Full.pdf
+        """
+        # Ensure we are composing two divisors
+        if not isinstance(other, MumfordDivisorSplit):
+            raise ValueError("TODO")
+
+        # Collect data from HyperellipticCurve
+        H = self.parent().curve()
+        f, h = H.hyperelliptic_polynomials()
+        g = H.genus()
+
+        u1, v1 = self.uv()
+        u2, v2 = other.uv()
+
+        # Ensure D1 and D2 are semi-reduced divisors
+        assert (
+            v1.degree() < u1.degree() and v2.degree() < u2.degree()
+        ), "The degree of bi must be smaller than ai"
+        assert (
+            u1.degree() <= f.degree() and u2.degree() <= f.degree()
+        ), f"The degree of ai must be smaller than f, {u1.degree()}, {u2.degree()}"
+        assert (v1**2 + v1 * h - f) % u1 == 0, "D1 is not a valid divisor"
+        assert (v2**2 + v2 * h - f) % u2 == 0, "D2 is not a valid divisor"
+
+        # Step One
+        s0, e1, e2 = u1.xgcd(u2)
+        assert s0 == e1 * u1 + e2 * u2
+
+        # Step Two
+        s, c1, c2 = s0.xgcd(v1 + v2 + h)
+        assert s == c1 * s0 + c2 * (v1 + v2 + h)
+
+        # Step Three
+        f1 = c1 * e1
+        f2 = c1 * e2
+        f3 = c2
+        assert s == f1 * u1 + f2 * u2 + f3 * (v1 + v2 + h)
+
+        # Step Four
+        u = (u1 * u2) // (s**2)
+        v = (f1 * u1 * v2 + f2 * u2 * v1 + f3 * (v1 * v2 + f))
+
+        # TODO:
+        # Sometimes I cannot invert d mod a, but it seems like in these cases
+        # I can always just divide it out (at least experimentally). Potentially
+        # edge cases...
+        if s.divides(v):
+            v = (v // s) % u
+        else:
+            d_inverse = s.inverse_mod(u)
+            v = (v * d_inverse) % u
+        return (u, v), (s.degree(), s.degree())
+
+    def cantor_reduction(self):
+        """
+        Follows algorithm 3.5 of
+
+        Efficient Arithmetic on Hyperelliptic Curves With Real Representation
+        David J. Mireles Morales (2008)
+        https://www.math.auckland.ac.nz/~sgal018/Dave-Mireles-Full.pdf
+        """
+        # Collect data from HyperellipticCurve
+        H = self.parent().curve()
+        f, h = H.hyperelliptic_polynomials()
+        g = H.genus()
+
+        # Mumford polynomials
+        u0, v0 = self.uv()
+
+        # Ensure D is a semi-reduced divisor
+        assert u0.degree() >= g + 2, "Divisor has incorrect degree"
+        assert (v0**2 + v0 * h - f) % u0 == 0, "D is not a valid divisor"
+
+        # Compute u' and v'
+        u1 = (f - v0 * h - v0**2) // u0
+        u1 = u1.monic()
+        v1 = (-h - v0) % u1
+        D1 = self.parent()(u1, v1)
+
+        # Compute the counter weights
+        d0 = u0.degree()
+        d1 = u1.degree()
+        a_plus, a_minus = self.parent().curve().roots_at_infinity()
+
+        if v0.leading_coefficient() == a_plus:
+            omega_plus, omega_minus = (d0 - g - 1, g + 1 - d1)
+        elif v0.leading_coefficient() == a_minus:
+            omega_plus, omega_minus = (g + 1 - d1, d0 - g - 1)
+        else:
+            omega = (d0 - d1) // 2
+            omega_plus, omega_minus = (omega, omega)
+        return D1, (omega_plus, omega_minus)
+
+
+    def cantor_reduction_at_infinity(self):
+        pass
---
+++ hyperelliptic_split.py
@@ -0,0 +1,289 @@
+from sage.misc.prandom import choice
+from sage.rings.polynomial.polynomial_element import Polynomial
+from sage.rings.polynomial.polynomial_ring_constructor import PolynomialRing
+from sage.rings.integer_ring import ZZ
+
+class HyperellipticCurveSplit:
+    def __init__(self, f, h=0):
+        # Some values which we will cache as a user asks for them
+        self._alphas = None
+        self._genus = None
+        self._projective_model = None
+
+        # Check the curve is well formed
+        disc = h**2 + 4*f
+        if not isinstance(disc, Polynomial):
+            raise TypeError(f"arguments {f = } and {h = } must be polynomials")
+        self._polynomial_ring = disc.parent()
+        self._base_ring = disc.base_ring()
+
+        # TODO: check singularity from disc
+
+        # Store the hyperelliptic polynomials as the correct type
+        f = self._polynomial_ring(f)
+        h = self._polynomial_ring(h)
+        self._hyperelliptic_polynomials = (f, h)
+        self._d = max(h.degree(), (f.degree() / 2).ceil())
+
+        # Class which represents points
+        self._point = HyperellipticPoint
+
+    def __repr__(self):
+        f, h = self._hyperelliptic_polynomials
+        if h:
+            if h.is_one():
+                curve = f"y^2 + y = {f}"
+            else:
+                curve = f"y^2 + y*{h} = {f}"
+        else:
+            curve = f"y^2 = {f}"
+        return f"Hyperelliptic Curve over {self.base_ring()} defined by {curve}"
+
+    def genus(self):
+        """
+        Compute the genus of the hyperelliptic curve
+        """
+        if self._genus:
+            return self._genus
+
+        f, h = self._hyperelliptic_polynomials
+        df = f.degree()
+        dh_2 = 2*h.degree()
+        if dh_2 < df:
+            self._genus = (df-1)//2
+        else:
+            self._genus = (dh_2-1)//2
+
+        return self._genus
+
+    def base_ring(self):
+        return self._base_ring
+
+    def polynomial_ring(self):
+        return self._polynomial_ring
+
+    def projective_model(self):
+        """
+        Compute the weighted projective model (1 : 3 : 1)
+        """
+        if self._projective_model:
+            return self._projective_model
+
+        f, h = self._hyperelliptic_polynomials
+        R, (X, Y, Z) = PolynomialRing(self.base_ring(), names="X, Y, Z").objgens()
+
+        if h is None:
+            d = f.degree()
+            F = sum(f[i] * X**i * Z**(d - i) for i in range(d + 1))
+            return Y**2 - F
+
+        d = max(h.degree(), (f.degree() / 2).ceil())
+        H = sum(h[i] * X**i * Z**(d - i) for i in range(d + 1))
+        F = sum(f[i] * X**i * Z**(2*d - i) for i in range(2*d + 1))
+        self._projective_model = Y**2 + H*Y - F
+        return self._projective_model
+
+    def point(self, coords, check=True):
+        if len(coords) == 2:
+            X, Y = coords
+            Z = self.base_ring().one()
+        elif len(coords) == 3:
+            X, Y, Z = coords
+        else:
+            raise ValueError("TODO")
+
+        if check:
+            F = self.projective_model()
+            if not F(X, Y, Z).is_zero():
+                raise ValueError("TODO")
+
+        return self._point(X, Y, Z)
+
+    def hyperelliptic_polynomials(self):
+        """
+        Return the polynomials (f, h) such that
+        C : y^2 + h*y = f
+        """
+        return self._hyperelliptic_polynomials
+
+    def roots_at_infinity(self):
+        """
+        Compute the roots of: Y^2 + h[d]Y - d[2d] = 0
+        When the curve is ramified, we expect one root, when
+        the curve is split or inert we expect zero or two roots.
+        """
+        if self._alphas:
+            return self._alphas
+
+        f, h = self._hyperelliptic_polynomials
+        x = f.parent().gen()
+        if not h:
+            return f.leading_coefficient().sqrt(all=True)
+
+        d = self._d
+        self._alphas = (x**2 + x * h[d] - f[2*d]).roots(multiplicities=False)
+        return self._alphas
+
+    def infinite_polynomials(self):
+        """
+        TODO: stupid name
+
+        Computes G^±(x) for curves in the split degree model
+        """
+        alphas = self.roots_at_infinity()
+
+        # This function only makes sense for the split model
+        if not len(alphas) == 2:
+          raise ValueError("hyperelliptic curve does not have the split model")
+
+        f, h = self._hyperelliptic_polynomials
+        alpha_plus, alpha_minus = alphas
+        d = self._d
+
+        # Construct G_plus from alpha_plus
+        g = [None] * (d + 1)
+        g[d] = alpha_plus
+        for i in range(d - 1, -1, -1):
+            # You just want (g * (g + h))[x^(i + d)] to match f_{i + d}
+            the_rest = g[d] * h[i] + sum(g[k] * (g[i + d - k] + h[i + d - k]) for k in range(i + 1, d))
+            g[i] = (f[i + d] - the_rest) / (2 * g[d] + h[d])
+
+        G_plus = self._polynomial_ring(g)
+        G_minus = - G_plus - h
+
+        return G_plus, G_minus
+
+    def points_at_infinity(self):
+        """
+        Compute the points at infinity on the curve. Assumes we are using
+        a weighted projective model for the curve
+        """
+        # TODO: check to False
+        return [self.point([1, y, 0], check=True) for y in self.roots_at_infinity()]
+
+    def is_x_coord(self, x):
+        """
+        Return True if ``x`` is the `x`-coordinate of a point on this curve.
+        """
+        f, h = self.hyperelliptic_polynomials()
+        K = self.base_ring()
+        try:
+            x = K(x)
+        except (ValueError, TypeError):
+            raise TypeError('x must be coercible into the base ring of the curve')
+
+        # When h is zero then x is a valid coordinate if y2 is square
+        if not h:
+            y2 = f(x)
+            return y2.is_square()
+        # Generic case for h != 0
+        a = f(x)
+        b = h(x)
+        # Special case for char 2
+        if K.characteristic() == 2:
+            R = f.parent()  # Polynomial ring K[x]
+            F = R([-a, b, 1])
+            return bool(F.roots())
+        # Otherwise x is a point on the curve if the discriminant is a square
+        D = b*b + 4*a
+        return D.is_square()
+
+    def lift_x(self, x, all=False):
+        """
+        Return one or all points with given `x`-coordinate.
+
+        This method is deterministic: It returns the same data each
+        time when called again with the same `x`.
+        """
+        f, h = self._hyperelliptic_polynomials
+        K = self.base_ring()
+
+        # Compute the common parent between the base ring of the curve and
+        # the parent of the input x-coordinate.
+        from sage.structure.element import get_coercion_model
+        cm = get_coercion_model()
+        try:
+            L = cm.common_parent(x.parent(), K)
+            x = L(x)
+        except (TypeError, ValueError):
+            raise ValueError('x must have a common parent with the base ring')
+
+        # First we compute the y-coordinates the given x-coordinate
+        ys = []
+        one = L.one()
+
+        # When h is zero we find all y-coordinates with a single sqrt
+        if not h:
+            y2 = f(x)
+            # When y2 is not a square, ys will be an empty list
+            ys = y2.sqrt(all=True, extend=False)
+        # Otherwise we need roots of the discriminant
+        else:
+            a = f(x)
+            b = h(x)
+            # Special case for char 2
+            if K.characteristic() == 2:
+                R = f.parent()
+                F = R([-a, b, 1])
+                ys = F.roots(L, multiplicities=False)
+            else:
+                D = b*b + 4*a
+                # When D is not a square, ys will be an empty list
+                ys = [(-b+d)/2 for d in D.sqrt(all=True, extend=False)]
+
+        if ys:
+            ys.sort()  # Make lifting deterministic
+            if all:
+                return [self.point([x, y, one], check=False) for y in ys]
+            else:
+                return self.point([x, ys[0], one], check=False)
+
+        if all:
+            return []
+        else:
+            raise ValueError(f"No point with x-coordinate {x} on {self}")
+
+    def random_point(self):
+        """
+        Return a random point on this hyperelliptic curve, uniformly chosen
+        among all rational points.
+        """
+        k = self.base_ring()
+        n = 2 * k.order() + 1
+
+        while True:
+            # Choose the point at infinity with probability 1/(2q + 1)
+            i = ZZ.random_element(n)
+            if not i:
+                # TODO: deal with that there is more than one point at infinity
+                return choice(self.points_at_infinity())
+            v = self.lift_x(k.random_element(), all=True)
+            try:
+                return v[i % 2]
+            except IndexError:
+                pass
+
+    def __call__(self, coords):
+        return self.point(coords)
+
+    def jacobian(self):
+        from jacobian_split import JacobianSplit
+        return JacobianSplit(self)
+
+
+class HyperellipticPoint:
+    def __init__(self, X, Y, Z=1):
+        self._X = X
+        self._Y = Y
+        self._Z = Z
+
+    def xy(self):
+        if not self._Z:
+            raise ValueError("TODO")
+
+        x = self._X / self._Z
+        y = self._Y / self._Z
+        return x, y
+
+    def __repr__(self):
+        return f"({self._X} : {self._Y} : {self._Z})"
